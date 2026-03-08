2aacc2d73cccff827e68eee8cf1b7894a87bbf1a
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-03-08 21:26:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ for i, n in enumerate(numbers):
 
             recent_hits = []
 
-            start = max(w, len(engine)-25)
+            start = max(w, len(engine)-26)
 
             for j in range(start, len(engine)):
 
