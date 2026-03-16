b2def9521f95e1020c60126f68bf83369ccc4468
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-03-16 08:44:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from collections import Counter
 
 # ---------------- CONFIG ----------------
 DATA_URL = "https://docs.google.com/spreadsheets/d/18gQsFPYPHB2EtkY_GLllBYKWcFPi_VP1vtGatflAuuY/export?format=csv"
-SCAN = 168
+SCAN = 182
 WINDOW_MIN = 6
 WINDOW_MAX = 20
 GAP = 4
