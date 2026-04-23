7140da771a76cbbfe984557e009b9034f052c79e
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-04-23 07:20:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ MODES = [
     {"name": "5v4", "top_windows": 5, "vote_required": 4, "window_min": 6, "window_max": 22},
     {"name": "6v4", "top_windows": 6, "vote_required": 4, "window_min": 6, "window_max": 22},
     {"name": "8v5", "top_windows": 8, "vote_required": 5, "window_min": 6, "window_max": 22},
+    {"name": "7v5", "top_windows": 7, "vote_required": 4, "window_min": 6, "window_max": 22},
 ]
 
 GAP = 1
