50ca3be42611e26dd0cf8662a1ecc87b84ac4e20
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-04-25 08:11:35 +0700

    Change auto-refresh interval to 10000ms
    Increased the auto-refresh interval from 3000ms to 10000ms.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 
 # ================= REFRESH =================
-st_autorefresh(interval=3000, key="refresh")
+st_autorefresh(interval=10000, key="refresh")
 
 # ================= CONFIG =================
 SHEET_ID = "18gQsFPYPHB2EtkY_GLllBYKWcFPi_VP1vtGatflAuuY"
