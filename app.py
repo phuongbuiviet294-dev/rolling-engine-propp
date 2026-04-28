8d65f598fa6b6c15993cf2518e825ea909ac41ec
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-04-28 11:11:55 +0700

    Change vote_required from 5 to 4 for 7v4 mode

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ LOCK_ROUND_END = 180
 MODES = [
     {"name": "4v3", "top_windows": 4, "vote_required": 3, "window_min": 6, "window_max": 26},
     {"name": "6v4", "top_windows": 6, "vote_required": 4, "window_min": 6, "window_max": 26},
-    {"name": "7v5", "top_windows": 7, "vote_required": 5, "window_min": 6, "window_max": 26},
+    {"name": "7v4", "top_windows": 7, "vote_required": 4, "window_min": 6, "window_max": 26},
    {"name": "8v5", "top_windows": 8, "vote_required": 5, "window_min": 6, "window_max": 26}, 
     
 ]
