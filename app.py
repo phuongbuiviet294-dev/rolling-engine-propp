50372d417a0faf17d96edd35c347f3105511e0c9
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-04-29 18:47:07 +0700

    Update MODES configuration and RELock scan length

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ LOCK_ROUND_START = 168
 LOCK_ROUND_END = 180
 
 MODES = [
-    {"name": "5v4", "top_windows": 5, "vote_required": 4, "window_min": 6, "window_max": 26},
+    {"name": "4v3", "top_windows": 4, "vote_required": 3, "window_min": 6, "window_max": 26},
     {"name": "6v4", "top_windows": 6, "vote_required": 4, "window_min": 6, "window_max": 26},
-    {"name": "7v4", "top_windows": 7, "vote_required": 4, "window_min": 6, "window_max": 26},
+
    {"name": "8v5", "top_windows": 8, "vote_required": 5, "window_min": 6, "window_max": 26}, 
     
 ]
@@ -56,7 +56,7 @@ MIN_TRAIN_LEN = 120
 MIN_VALIDATE_TRADES = 2
 VALIDATE_MIN_DRAWDOWN = -6.0
 
-RELOCK_SCAN_LEN = 12
+RELOCK_SCAN_LEN = 7
 RELOCK_BUFFER = 0
 
 REPLAY_FROM = 180
