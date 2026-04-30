576c541bc99fe4806495cec86f65b0609925822f
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-04-30 14:59:22 +0700

    Adjust mode configurations and minimum window spacing

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ LOCK_ROUND_END = 180
 
 MODES = [
     {"name": "4v3", "top_windows": 4, "vote_required": 3, "window_min": 6, "window_max": 26},
-    {"name": "5v4", "top_windows": 5, "vote_required": 4, "window_min": 6, "window_max": 26},
+   
     {"name": "6v4", "top_windows": 6, "vote_required": 4, "window_min": 6, "window_max": 26},
-    {"name": "7v4", "top_windows": 7, "vote_required": 4, "window_min": 6, "window_max": 26},
+
     {"name": "8v5", "top_windows": 8, "vote_required": 5, "window_min": 6, "window_max": 26},
 ]
 
@@ -48,7 +48,7 @@ KEEP_AFTER_LOSS_ROUNDS = 1
 
 MIN_TRADES_PER_WINDOW = 16
 RECENT_WINDOW_SIZE = 26
-MIN_WINDOW_SPACING = 5
+MIN_WINDOW_SPACING = 1
 MAX_CANDIDATE_WINDOWS = 10
 
 VALIDATE_LEN = 24
