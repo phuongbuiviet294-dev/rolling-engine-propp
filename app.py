78b5e11520147f97684ab53d9df2b132c0c85f79
phuongbuiviet294-dev <phuongbuiviet294@gmail.com>
2026-06-07 21:03:11 +0700

    Modify MODES list in app.py
    Commented out the '6v3' mode and added '8v4' mode to the MODES list.

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ REPLAY_FROM = 180
 
 MODES = [
   #  {"name": "5v3", "top_windows": 5, "vote_required": 3, "window_min": 6, "window_max": 22},
-    {"name": "6v3", "top_windows": 6, "vote_required": 3, "window_min": 6, "window_max": 22},
-  #  {"name": "8v4", "top_windows": 8, "vote_required": 4, "window_min": 6, "window_max": 22},
+   # {"name": "6v3", "top_windows": 6, "vote_required": 3, "window_min": 6, "window_max": 22},
+    {"name": "8v4", "top_windows": 8, "vote_required": 4, "window_min": 6, "window_max": 22},
   #  {"name": "8v5", "top_windows": 8, "vote_required": 5, "window_min": 6, "window_max": 22},
  #   {"name": "10v6", "top_windows": 10, "vote_required": 6, "window_min": 8, "window_max": 30},
 ]
